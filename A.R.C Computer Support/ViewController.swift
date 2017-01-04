06d6ce659f6c92b90c96a3f1a193b4ecc4fa2d57
William Wright <william86370@gmail.com>
2017-01-04 06:42:36 +0300

    code upload

---
+++ A.R.C Computer Support/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A.R.C Computer Support
+//
+//  Created by william wright  on 5/8/16.
+//  Copyright © 2016 A.R.C software and enginering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
